84d2fab1912d742399e67e26370d2ccb1966bded
Ivan Kondrashkov <vanya.kondrashkov.92@mail.ru>
2024-10-08 16:46:45 +0300

    feat: Добавлена библиотека jenkins-lib

--- src/ModuleUtils.groovy
+++ src/ModuleUtils.groovy
@@ -2,7 +2,7 @@ import java.time.LocalDateTime
 
 class ModuleUtils {
 
-    def getModuleBuildDir(branch) {
+    def getModuleBuildDir(String branch) {
         def baseDir = "build/libs/*.jar"
         switch (branch) {
             case "homework-1": return "hw01-gradle/${baseDir}"
@@ -11,10 +11,12 @@ class ModuleUtils {
             case "homework-4": return "hw04-json-gradle/${baseDir}"
             case "homework-6": return "hw06-xlsx-gradle/${baseDir}"
             case "homework-8": return "hw08-dsl-gradle/${baseDir}"
+            default: return baseDir
         }
     }
 
-    def getModuleName(branch) {
+    def getModuleName(String branch) {
+        def baseName = "groovy-2024-08"
         switch (branch) {
             case "homework-1": return "hw01-gradle"
             case "homework-2": return "hw02-gradle"
@@ -22,19 +24,15 @@ class ModuleUtils {
             case "homework-4": return "hw04-json-gradle"
             case "homework-6": return "hw06-xlsx-gradle"
             case "homework-8": return "hw08-dsl-gradle"
+            default: return baseName
         }
     }
 
-    def printSysTime(branch) {
-        return {
-            echo "${LocalDateTime.now()} | Build branch ${branch}}"
-        }
+    def printSysTime(String branch) {
+        return "${LocalDateTime.now()} | Build branch ${branch}"
     }
 
-   def countFilesModule(branch) {
-        return dir("${getModuleName(branch)}/") {
-            def files = findFiles(glob: '**')
-            echo "Files module - ${files[0].length}"
-        }
+    def isRootProject(String module) {
+        return module == 'groovy-2024-08'
     }
 }
--- vars/transformBuildAndPushDocker.groovy
+++ vars/transformBuildAndPushDocker.groovy
@@ -1,15 +1,21 @@
-def call(String branch) {
+def call(String baseUrl, String branch) {
     def moduleUtils = library('jenkins-lib').ModuleUtils.new()
     def module = moduleUtils.getModuleName(branch)
+    def isRootProject = moduleUtils.isRootProject(module)
     return  {
-        withCredentials([usernamePassword(
-                credentialsId: 'docker-hub',
-                usernameVariable: 'DOCKER_HUB_USERNAME_TOKEN',
-                passwordVariable: 'DOCKER_HUB_PASSWORD_TOKEN'
-        )]) {
-            sh 'docker login -u $DOCKER_HUB_USERNAME_TOKEN -p $DOCKER_HUB_PASSWORD_TOKEN'
-            def image = docker.build("ivan92k/${module}:${env.BUILD_ID}", "-f ${module}/Dockerfile .")
-            image.push()
+        node {
+            git branch: branch, url: baseUrl
+            if (!isRootProject) {
+                withCredentials([usernamePassword(
+                        credentialsId: 'docker-hub',
+                        usernameVariable: 'DOCKER_HUB_USERNAME_TOKEN',
+                        passwordVariable: 'DOCKER_HUB_PASSWORD_TOKEN'
+                )]) {
+                    sh 'docker login -u $DOCKER_HUB_USERNAME_TOKEN -p $DOCKER_HUB_PASSWORD_TOKEN'
+                    def image = docker.build("ivan92k/${module}:${env.BUILD_ID}", "-f ${module}/Dockerfile .")
+                    image.push()
+                }
+            }
         }
     }
 }
